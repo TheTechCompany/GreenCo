19e51c5902bb94caa3a69185f96fd5d9b8ccda26
Ross Leitch <professional.balbatross@gmail.com>
2021-12-02 17:44:11 +1300

    Client

--- packages/green-dns/src/client.ts
+++ packages/green-dns/src/client.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import yargs from 'yargs/yargs'
+import { connect_data, disconnect_data, DNSRecord } from './data';
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 
@@ -14,26 +15,26 @@ let args : {
 if(args.register && !args.ip) throw new Error("Must provide IP");
 
 
-// const main = async () => {
-//     await connect_data();
+const main = async () => {
+    await connect_data();
 
-//     if(args.register){
-//         await DNSRecord.updateOne({domain: args.domain, subdomain: args.register}, {$set: {address: args.ip}}, {upsert: true})
+    if(args.register){
+        await DNSRecord.updateOne({domain: args.domain, subdomain: args.register}, {$set: {address: args.ip}}, {upsert: true})
             
-//         console.log(`Updated record ${args.register}.${args.domain} = ${args.ip}`)
+        console.log(`Updated record ${args.register}.${args.domain} = ${args.ip}`)
         
-//     }
+    }
 
-//     if(args.deregister){
-//         await DNSRecord.remove({domain: args.domain, subdomain: args.deregister})
+    if(args.deregister){
+        await DNSRecord.remove({domain: args.domain, subdomain: args.deregister})
             
-//         console.log(`Removed record ${args.deregister}.${args.domain}`)
+        console.log(`Removed record ${args.deregister}.${args.domain}`)
 
-//     }
+    }
 
-//     return await disconnect_data()
-// }
+    return await disconnect_data()
+}
 
-// main().then(() => {
-//     process.exit(0);
-// })
+main().then(() => {
+    process.exit(0);
+})
