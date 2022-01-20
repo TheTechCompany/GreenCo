070527564aee1f93eb38c0a76a45a13e839665b5
Ross Leitch <professional.balbatross@gmail.com>
2022-01-20 20:48:15 +1300

    Backend download

--- packages/green-cdn/src/routes/distribute/index.ts
+++ packages/green-cdn/src/routes/distribute/index.ts
@@ -6,10 +6,16 @@ export default (session: Session) => {
 
 	router.route('/')
 		.get(async (req, res) => {
+			let networkName = (req as any).user.hostname.replace('.hexhive.io', '');
+
 			const campaigns = await session.run(`
-				MATCH (campaigns:Campaign)
+				MATCH (campaigns:Campaign)<--(:Schedule)<--(:LocationGroup)-->(:Location)<--(:GreenScreen {networkName: $networkName})
+				WHERE campaigns.assetFolder IS NOT NULL
 				RETURN campaigns{.*}
-			`)
+			`, {
+				networkName
+			})
+			
 			res.send({
 				campaigns: campaigns?.records?.map(record => record.get(0))
 			})
