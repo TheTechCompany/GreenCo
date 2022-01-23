6397fdee34e311a9820f7213b18c21c34980f998
Ross Leitch <professional.balbatross@gmail.com>
2022-01-23 17:55:42 +1300

    Updated analytic views

--- packages/green-machine/src/plugins/index.ts
+++ packages/green-machine/src/plugins/index.ts
@@ -99,6 +99,7 @@ export class PluginManager {
 			let plugin_exec = path.join(plugin_path, `./index.py`)
 
 			let pid = spawn(`python3 ${plugin_exec}`, {cwd: plugin_path})
+			console.log("Spawn python process", {pid});
 
 			pid.stdout.on('data', (data) => {
 				console.log(`${plugin.name} stdout: ${data.toString()}`)	
