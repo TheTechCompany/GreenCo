00d736caf06120c5ee91617ee4f71ae9558a52d8
Connor Scott <connorscott@Connors-MacBook-Pro.local>
2022-02-17 14:17:30 +1300

    tidied up login form

--- packages/customer-dashboard/my-app/src/veiws/LoginForm.tsx
+++ packages/customer-dashboard/my-app/src/veiws/LoginForm.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { Form, FormField, TextInput, Box, Button } from "grommet";
-import { Hide, View } from 'grommet-icons';
+import { Hide, View } from "grommet-icons";
 
+export const LoginForm = () => {
+  const [value, setValue] = React.useState({});
+  return (
+    <Form
+      value={value}
+      onChange={(nextValue) => setValue(nextValue)}
+      onReset={() => setValue({})}
+      onSubmit={({ value }) => {}}
+      color="black"
+    >
+      <FormField name="name" htmlFor="text-input-id" label="Name">
+        <TextInput id="text-input-id" name="name" />
+      </FormField>
+      <FormField name="Password" htmlFor="text-input-id" label="Password">
+        <TextInput id="pass-input-id" name="password" type="password" />
+      </FormField>
 
-export const LoginForm =   
-    () => {
-        const [value, setValue] = React.useState({});
-        return (
-          <Form
-            value={value}
-            onChange={nextValue => setValue(nextValue)}
-            onReset={() => setValue({})}
-            onSubmit={({ value }) => {}}
-            color="black"
-          >
-            <FormField name="name" htmlFor="text-input-id" label="Name">
-              <TextInput id="text-input-id" name="name" />
-            </FormField>
-            <FormField name="Password" htmlFor="text-input-id" label="Password">
-            <TextInput id="pass-input-id" name="password" type="password"/>
-            </FormField>
-        
-            <Box direction="row" gap="medium">
-              <Button type="submit" primary label="Submit" color="black"/>
-              <Button type="reset" label="Reset" color="black" />
-            </Box>
-          </Form>
-        );
-        }
+      <Box direction="row" gap="medium">
+        <Button
+          type="submit"
+          primary
+          label="Submit"
+          color="black"
+          href="/dashboard"
+        />
+        <Button type="reset" label="Reset" color="black" />
+      </Box>
+    </Form>
+  );
+};
