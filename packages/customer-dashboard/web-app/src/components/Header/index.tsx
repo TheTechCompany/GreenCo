f40e8d0dc33b332f7877907d6748bd708df0200a
Ross Leitch <ross@hexhive.io>
2022-02-18 17:45:52 +1300

    Big reshuffle

---
+++ packages/customer-dashboard/web-app/src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Box, Image, Header, Button, Menu } from "grommet";
+import { UserSettings } from "grommet-icons";
+import { NavItem } from "./NavItem";
+
+// TO MAKE BUTTONS ACTIVE WHEN ON PAGE - I image it's a simple useState thing I need to figure out
+
+export const HeaderBar = () => {
+  const navOptions = [{label: "Campaigns", route: 'campaigns'}, {label: "Reports", route: 'reports'}]
+
+  return (
+    <Header
+      background="#000000"
+      margin="none"
+      pad={{ horizontal: "medium", vertical: "medium" }}
+      elevation="small"
+      justify="between"
+      height="50px"
+    >
+        <Image height="24px" src="/assets/sl1_logo.png" />
+      <Box
+        direction="row"
+        gap="small"
+        pad={{ horizontal: "medium" }}
+      >
+        {navOptions.map((nav) => (
+          <NavItem
+            route={nav.route}
+            label={nav.label} />
+        ))}
+       
+        <Box border={{size: 'small', color: 'gray'}} round="xsmall" elevation="small">
+          <Menu
+    
+            icon={<UserSettings />}
+            label="Settings"
+            items={[
+              { label: "Edit Profile", onClick: () => {} },
+              { label: "Change Password", onClick: () => {} },
+              { label: "Log Out", onClick: () => {} },
+            ]}
+          />
+        </Box>
+      </Box>
+    </Header>
+  )
+}
