ed64e3a1bf01175b44c870347dcb2cb2adcaf820
Connor Scott <connorscott@Connors-MacBook-Pro.local>
2022-02-20 11:35:41 +1300

    fixed login button margin

--- packages/customer-dashboard/web-app/src/views/LoginForm.tsx
+++ packages/customer-dashboard/web-app/src/views/LoginForm.tsx
@@ -2,17 +2,10 @@ import React from "react";
 import { Form, FormField, TextInput, Box, Button, Image, Text } from "grommet";
 import { Hide, View } from "grommet-icons";
 
-// NEED TO FIGURE OUT HOW TO CENTER THE HEADING ETC
-
 export const LoginForm = () => {
   const [value, setValue] = React.useState({});
   return (
-    <Box
-      flex
-      background="black"
-      align="center"
-      justify="center"
-    >
+    <Box flex background="black" align="center" justify="center">
       <Image height="28vh" src="/assets/sl1_logo.png" margin="medium" />
 
       <Box
@@ -23,32 +16,27 @@ export const LoginForm = () => {
         width="large"
         height="medium"
       >
+        <Box>
+          <Text size="xlarge" alignSelf="center" textAlign="center">
+            Username
+          </Text>
 
-          <Box>
-            <Text size="xlarge" alignSelf="center" textAlign="center">
-              Username
-            </Text>
-
-            <TextInput id="text-input-id" name="name"  />
-          </Box>
-          <Box>
-            <Text size="xlarge" alignSelf="center" textAlign="end">
-              Password
-            </Text>
-            <TextInput
-              id="pass-input-id"
-              name="password"
-              type="password"
-            />
-          </Box>
-          <Box direction="row" gap="medium">
-            <Button
-              type="submit"
-              label="Submit"
-              color="white"
-              href="/dashboard"
-            />
-          </Box>
+          <TextInput id="text-input-id" name="name" />
+        </Box>
+        <Box>
+          <Text size="xlarge" alignSelf="center" textAlign="end">
+            Password
+          </Text>
+          <TextInput id="pass-input-id" name="password" type="password" />
+        </Box>
+        <Box direction="row" gap="medium" margin="small">
+          <Button
+            type="submit"
+            label="Submit"
+            color="white"
+            href="/dashboard"
+          />
+        </Box>
         <Box
           direction="row"
           justify="between"
